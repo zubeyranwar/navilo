d846a1749ca6d7ad22a7475ecbace1047c758a22
zubeyranwar <zubeyranwar@gmail.com>
2025-09-15 23:33:18 +0300

    fix: align virtual module ID with import path

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const DEFAULT_OPTIONS = {
     typescript: true,
 };
 
-export const VIRTUAL_ROUTE_MODULE_ID = 'virtual:preluder-routes';
+export const VIRTUAL_ROUTE_MODULE_ID = 'virtual:navilo-routes';
 export const RESOLVED_VIRTUAL_ROUTE_MODULE_ID = '\0' + VIRTUAL_ROUTE_MODULE_ID;
